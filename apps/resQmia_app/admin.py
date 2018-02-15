0548180fefb86cb13cf877d78e0d9a454f8d9426
Bald Mike <baldmike312@gmail.com>
2018-02-15 01:07:13 -0600

    attempt pull

--- apps/resQmia_app/admin.py
+++ apps/resQmia_app/admin.py
@@ -3,4 +3,51 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import *
+
+class DogAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Dog
+
+class VaccineDogAdmin(admin.ModelAdmin):
+    class Meta:
+        model = VaccineDog
+
+class PreventionDogAdmin(admin.ModelAdmin):
+    class Meta:
+        model = VaccineDog
+
+class TestDogAdmin(admin.ModelAdmin):
+    class Meta:
+        model = TestDog
+
+
+class CatAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Cat
+
+class VaccineCatAdmin(admin.ModelAdmin):
+    class Meta:
+        model = VaccineCat
+
+class PreventionCatAdmin(admin.ModelAdmin):
+    class Meta:
+        model = VaccineCat
+
+class TestCatAdmin(admin.ModelAdmin):
+    class Meta:
+        model = TestCat
+
+
+
+
+admin.site.register(Dog, DogAdmin)
+admin.site.register(VaccineDog, VaccineDogAdmin)
+admin.site.register(PreventionDog, PreventionDogAdmin)
+admin.site.register(TestDog, TestDogAdmin)
+
+admin.site.register(Cat, CatAdmin)
+admin.site.register(VaccineCat, VaccineCatAdmin)
+admin.site.register(PreventionCat, PreventionCatAdmin)
+admin.site.register(TestCat, TestCatAdmin)
+
